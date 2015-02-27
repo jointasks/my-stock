09b531102aaf8df93620c9f31d44fb3693e4490f
jointasks <1769832362@qq.com>
2015-02-27 16:07:25 +0800

    ii

--- my-stock/src/test/java/com/jointasks/mystock/MyBatisTest.java
+++ my-stock/src/test/java/com/jointasks/mystock/MyBatisTest.java
@@ -16,40 +16,39 @@ import com.jointasks.mystock.model.Blog;
 import com.jointasks.mystock.model.BlogMapper;
 
 public class MyBatisTest {
-	
-	private static SqlSessionFactory sessionFactory;
-	
-	@BeforeClass
-	public static void init() {
-		String resource = "mybatis-config.xml";
-		try {
-			InputStream is = Resources.getResourceAsStream(resource);
-			sessionFactory = new SqlSessionFactoryBuilder().build(is);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-	}
-	
-	@Test
-	public void testSelectBLog() {
-		try {
-			SqlSession session = sessionFactory.openSession();
-			
-			BlogMapper mapper = session.getMapper(BlogMapper.class);
-			
-			Blog blog= mapper.selectBlog(111);
-			
-			Assert.assertNotNull(blog);
-			session.close();
-		} catch(PersistenceException ex) {
-			ex.printStackTrace();
-		}
-		 catch(Exception ex) {
-				ex.printStackTrace();
-			}
-	}
-	
+
+    private static SqlSessionFactory sessionFactory;
+
+    @BeforeClass
+    public static void init() {
+        String resource = "mybatis-config.xml";
+        try {
+            InputStream is = Resources.getResourceAsStream(resource);
+            sessionFactory = new SqlSessionFactoryBuilder().build(is);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void testSelectBLog() {
+        try {
+            SqlSession session = sessionFactory.openSession();
+
+            BlogMapper mapper = session.getMapper(BlogMapper.class);
+
+            Blog blog = mapper.selectBlog(111);
+
+            Assert.assertNotNull(blog);
+            session.close();
+        } catch (PersistenceException ex) {
+            ex.printStackTrace();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
 
 }
